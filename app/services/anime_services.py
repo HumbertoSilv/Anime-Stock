56f4435765d5fbeab1200faa7ae0d2c6d83cb34a
Humberto <matheushumberto520@gmail.com>
2021-09-23 07:39:29 -0400

    Feat: get_all and get_by_id created

--- app/services/anime_services.py
+++ app/services/anime_services.py
@@ -1,4 +1,5 @@
 import psycopg2
+from app.exc.exc import AnimeNotFound
 from app.services.config import configs
 from psycopg2 import sql
 
@@ -65,4 +66,56 @@ class Animes():
 
         for_dict = dict(zip(keys, fetch_result))
 
+        return for_dict
+
+    @staticmethod
+    def get_all():
+        keys = ["id", "anime", "released_date", "seasons"]
+        create_table()
+
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        query = sql.SQL(
+            """
+                SELECT
+                    *
+                FROM
+                    animes;
+            """
+        )
+
+        cur.execute(query)
+        fetch_result = cur.fetchall()
+        close_connection(conn, cur)
+
+        for_dict = [
+            dict(zip(keys, value)) for value in fetch_result
+        ]
+        return {"data": for_dict}
+
+    @staticmethod
+    def get_by_id(anime_id: int):
+        keys = ["id", "anime", "released_date", "seasons"]
+
+        conn = psycopg2.connect(**configs)
+        cur = conn.cursor()
+
+        cur.execute(
+            """
+                SELECT
+                    *
+                FROM
+                    animes
+                WHERE
+                    id=(%s);
+            """, (anime_id, )
+        )
+        fetch_result = cur.fetchone()
+        if not fetch_result:
+            raise AnimeNotFound
+
+        close_connection(conn, cur)
+
+        for_dict = {"data": dict(zip(keys, fetch_result))}
         return for_dict
--- app/views/anime_views.py
+++ app/views/anime_views.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from app.services.anime_services import Animes
 from psycopg2.errors import UniqueViolation
+from app.exc.exc import AnimeNotFound
 
 
 bp_animes = Blueprint("anime_bp", __name__, url_prefix="/animes")
@@ -17,15 +18,23 @@ def get_create():
 
         except TypeError as e:
             return {"available_key": ["anime", "released_date", "seasons"],
-                    "wrong_keys_sended": [str(e).split("'")[1]]}, 422
+                    "wrong_keys_sended": [str(e).split("'")[1]]
+                    }, 422
 
         except UniqueViolation:
             return {"error": "anime already exists."}, 409
 
+    return Animes.get_all(), 200
+
 
 @bp_animes.get("/<int:anime_id>")
-def filter():
-    ...
+def filter(anime_id: int):
+    try:
+        anime = Animes.get_by_id(anime_id)
+        return anime, 200
+
+    except AnimeNotFound:
+        return {"error": "Not found."}, 404
 
 
 @bp_animes.patch("/<int:anime_id>")
